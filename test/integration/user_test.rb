4aed4cab932d7ed2a129b43507be1ab5e40c11ad
James Youn <jamesyoun710@gmail.com>
2015-10-30 14:16:24 -0700

    Update index view & tests to display better info

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -22,25 +22,25 @@ class UsersTest < ActionDispatch::IntegrationTest
   test 'employer can create a new job' do
     login_as(@employer)
     visit new_job_path
-    assert page.has_content?('Title')
-    fill_in('Title', with: 'Clean 3 bedroom house')
+    assert page.has_content?('New Job')
+    check 'cleaning'
     fill_in('Location', with: '1234 Main st')
     fill_in('First start time choice', with: '10/01/2015 1:00 PM')
     fill_in('Second start time choice', with: '10/17/2015 12:00 AM')
     find_button('Create Job')
     click_on 'Create Job'
-    assert page.has_content?('Clean 3 bedroom house'), 'job not created'
+    assert page.has_content?('cleaning'), 'job not created'
   end
 
   test 'update job as employer' do
     login_as(@employer)
     visit jobs_path
-    assert page.has_content?('MyString')
+    assert page.has_content?('Listing Jobs'), 'not on index page'
     first(:link, "Edit").click
-    assert page.has_content?('Title'), 'not on edit page'
-    fill_in('Title', with: 'Hello Test')
+    assert page.has_content?('Location'), 'not on edit page'
+    fill_in('Location', with: 'New Address Here')
     click_on('Update Job')
-    assert page.has_content?('Hello Test'), 'page not updated'
+    assert page.has_content?('New Address Here'), 'page not updated'
   end
 
 end
