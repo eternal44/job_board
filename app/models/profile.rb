8499689b17f3632604202375777f4d6d32ef9125
James Youn <jamesyoun710@gmail.com>
2015-11-02 11:50:23 -0800

    Annoate models to reflect job model changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,11 +2,12 @@
 #
 # Table name: profiles
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  name         :string
+#  user_id      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  phone_number :string
 #
 
 class Profile < ActiveRecord::Base
--- test/models/job_type_test.rb
+++ test/models/job_type_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: job_types
+#
+#  id         :integer          not null, primary key
+#  job_type   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class JobTypeTest < ActiveSupport::TestCase
