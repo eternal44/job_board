45aed39a35bcc536af4b4454654e4cac1d34d7c3
James Youn <jamesyoun710@gmail.com>
2015-11-01 20:39:09 -0800

    Allow ransack searches on admin dash

--- app/controllers/admin_dashboards_controller.rb
+++ app/controllers/admin_dashboards_controller.rb
@@ -1,18 +1,18 @@
 class AdminDashboardsController < ApplicationController
   def index
     if current_user.role == 'Attendant'
-      job_scope = Job.all.where(attendant_id: current_user.id)
+      @q = Job.all.where(attendant_id: current_user.id).ransack(params[:q])
     elsif current_user.role == 'Admin'
-      job_scope = Job.all
+      @q = Job.all.ransack(params[:q])
     else
       redirect_to root_url
     end
       # extract to model later
-      @jobs_pending = job_scope.where(status: 'Pending')
-      @jobs_approved = job_scope.where(status: 'Approved')
-      @jobs_open = job_scope.where(status: 'Open')
-      @jobs_finished = job_scope.where(status: 'Finished')
-      @jobs_closed = job_scope.where(status: 'Closed')
+      @jobs_pending = @q.result.includes(:user).where(status: 'Pending')
+      @jobs_approved = @q.result.includes(:user).where(status: 'Approved')
+      @jobs_open = @q.result.includes(:user).where(status: 'Open')
+      @jobs_finished = @q.result.includes(:user).where(status: 'Finished')
+      @jobs_closed = @q.result.includes(:user).where(status: 'Closed')
       @jobs = Job.all
   end
 end
