f31a2ffe10d7ca5958f8c0718c01a93ece415553
James Youn <jamesyoun710@gmail.com>
2015-11-10 14:58:34 -0800

    Update integration test for fav icons
    Remember:  You have to use selenium to select fav icons.

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -6,7 +6,7 @@ class UsersTest < ActionDispatch::IntegrationTest
   def setup
     @employer = users(:employer)
     @job = jobs(:one)
-    # Capybara.default_driver = :selenium
+    Capybara.default_driver = :selenium
   end
 
   test 'employers can login' do
@@ -35,9 +35,11 @@ class UsersTest < ActionDispatch::IntegrationTest
     login_as(@employer)
     visit jobs_path
     assert page.has_content?('Listing Jobs'), 'not on index page'
-    first(:link, "Edit").click
-    assert page.has_content?('Location'), 'not on edit page'
+    find(:css, 'i.fa.fa-search').click
+    assert page.has_content?('Edit Job'), 'not on edit page'
+    click_on('Edit Job')
     fill_in('Location', with: 'New Address Here')
+    fill_in('Appointment date time', with: 'January 2, 2016 5:00 PM')
     click_on('Update Job')
     assert page.has_content?('New Address Here'), 'page not updated'
   end
